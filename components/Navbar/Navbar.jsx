11f8383922392a092a814a3bb8637698b9c24a47
afisman <afisman@gmail.com>
2023-07-16 16:04:25 +0200

    searchbar fixed

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -98,8 +98,8 @@ const Navbar = () => {
  height={100}/>
 */}
                 </div>
-                <div className={Style.navigation_container_left_box_input}>
-                    <div className={Style.navigation_container_left_box_input_box}>
+                <div className={Style.navbar_container_left_box_input}>
+                    <div className={Style.navbar_container_left_box_input_box}>
                         <input type='text' placeholder='Search NFT' />
                         <BsSearch onClick={() => { }} className={Style.search_icon} />
                     </div>
