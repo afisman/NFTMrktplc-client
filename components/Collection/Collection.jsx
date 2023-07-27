088eec525d50c33ea98a87ebad04a74208a570c7
afisman <afisman@gmail.com>
2023-07-27 13:09:38 +0200

    finished collection component, started on daysComponent

--- components/Collection/Collection.jsx
+++ components/Collection/Collection.jsx
@@ -15,15 +15,27 @@ const Collection = () => {
     const newsArray = [1, 2, 3, 4, 5, 6];
 
     const openPopular = () => {
-
+        if (!popular) {
+            setPopular(true);
+            setFollowing(false);
+            setNews(false);
+        }
     }
 
     const openNews = () => {
-
+        if (!news) {
+            setPopular(false);
+            setFollowing(false);
+            setNews(true);
+        }
     }
 
     const openFollower = () => {
-
+        if (!following) {
+            setPopular(false);
+            setFollowing(true);
+            setNews(false);
+        }
     }
 
     return (
@@ -33,17 +45,47 @@ const Collection = () => {
                 <div className={Style.collection_collections}>
                     <div className={Style.collection_collections_btn}>
                         <button onClick={() => openPopular()}>
-                            <BsFillAlarmFill /> Last 24 hours
+                            <BsFillAlarmFill /> 24 hours
                         </button>
                         <button onClick={() => openFollower()}>
-                            <BsFillAlarmFill /> Last 7 days
+                            <BsCalendar3 /> 7 days
                         </button>
                         <button onClick={() => openNews()}>
-                            <BsCalendarDateFill /> Last 30 days
+                            <BsCalendarDateFill /> 30 days
                         </button>
                     </div>
                 </div>
             </div>
+
+            {
+                popular && (
+                    <div className={Style.collection_box}>
+                        {CardArray.map((el, i) => (
+                            <DaysComponents key={i + 1} />
+                        ))}
+                    </div>
+                )
+            }
+
+            {
+                following && (
+                    <div className={Style.collection_box}>
+                        {followingArray.map((el, i) => (
+                            <DaysComponents key={i + 1} />
+                        ))}
+                    </div>
+                )
+            }
+
+            {
+                news && (
+                    <div className={Style.collection_box}>
+                        {newsArray.map((el, i) => (
+                            <DaysComponents key={i + 1} />
+                        ))}
+                    </div>
+                )
+            }
         </div>
     )
 }
--- components/Collection/DaysComponents/DaysComponents.jsx
+++ components/Collection/DaysComponents/DaysComponents.jsx
@@ -1,10 +1,73 @@
 import React from 'react';
+import Image from 'next/image';
+import { MdVerified } from 'react-icons/md';
 
 import Style from './DaysComponents.module.css';
+import images from '../../../img';
 
 const DaysComponents = () => {
     return (
-        <div>DaysComponents</div>
+        <div className={Style.daysComponent}>
+            <div className={Style.daysComponent_box}>
+                <div className={Style.daysComponent_box_img}>
+                    <Image src={images.nft_image_2}
+                        className={Style.daysComponent_box_img_img}
+                        alt='Profile background'
+                        width={500}
+                        height={300}
+                    // objectFit='cover'
+                    />
+                </div>
+                <div className={Style.daysComponent_box_profile}>
+                    <Image
+                        src={images.nft_image_1}
+                        alt='profile'
+                        width={200}
+                        height={200}
+                        className={Style.daysComponent_box_img_1}
+                    // objectFit='cover'
+                    />
+                    <Image
+                        src={images.nft_image_1}
+                        alt='profile'
+                        width={200}
+                        height={200}
+                        className={Style.daysComponent_box_img_2}
+                    // objectFit='cover'
+                    />
+                    <Image
+                        src={images.nft_image_1}
+                        alt='profile'
+                        width={200}
+                        height={200}
+                        className={Style.daysComponent_box_img_3}
+                    // objectFit='cover'
+                    />
+
+                </div>
+                <div className={Style.daysComponent_box_title}>
+                    <h2>Amazing Collection</h2>
+                    <div className={Style.daysComponent_box_title_info}>
+                        <div className={Style.daysComponent_box_title_info_profile}>
+                            <Image
+                                src={images.user1}
+                                alt='Profile image'
+                                width={30}
+                                height={30}
+                                className={Style.daysComponent_box_title_info_profile_img}
+                            />
+                            <p>Creator
+                                <span>Alef Tau</span>
+                                <small><MdVerified /></small>
+                            </p>
+                        </div>
+                        <div className={Style.daysComponent_box_title_info_profile}>
+                            <small>1.333 ETH</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
