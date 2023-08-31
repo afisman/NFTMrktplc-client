86ce17201bfe158fb4c73a21b6f975111c7859c1
afisman <afisman@gmail.com>
2023-08-31 20:28:35 +0200

    worked on author page

--- authorPage/AuthorTaps/AuthorTaps.jsx
+++ authorPage/AuthorTaps/AuthorTaps.jsx
@@ -8,8 +8,46 @@ const AuthorTaps = ({ setCollectibles, setCreated, setLike, setFollower, setFoll
     const [openList, setOpenList] = useState(false);
     const [active, setActive] = useState(0);
     const [selectedMenu, setSelectedMenu] = useState("Most Recent");
+
+    const listArray = [
+        "Created By Admin",
+        "Most Appreciated",
+        "Most Discussed",
+        "Most Viewed",
+    ]
     return (
-        <div>AuthorTaps</div>
+        <div className={Style.autorTaps}>
+            <div className={Style.autorTaps_box}>
+                <div className={Style.autorTaps_box_left}>
+                    <div className={Style.autorTaps_box_left_btn}>
+                        <button
+                            onClick={() => openTab(e)}
+                            className={`${active == 1 ? Style.active : ""}`}
+                        >
+                            Collectibles{""}
+                        </button>
+                        <button
+                            onClick={() => openTab(e)}
+                            className={`${active == 2 ? Style.active : ""}`}
+                        >
+                            Created{""}
+                        </button>
+                        <button
+                            onClick={() => openTab(e)}
+                            className={`${active == 3 ? Style.active : ""}`}
+                        >
+                            Liked{""}
+                        </button>
+                        <button
+                            onClick={() => openTab(e)}
+                            className={`${active == 4 ? Style.active : ""}`}
+                        >
+                            Following{""}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
